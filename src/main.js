dcf592e5cf1f2e1280723f7b014cc344691c8824
Jordi Gomis <jgc.jordi@gmail.com>
2019-12-19 18:51:17 +0100

    theme added

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css' // el tema
+import './theme.scss'
 
 Vue.use(VueMaterial) // importamos el plugin
 
