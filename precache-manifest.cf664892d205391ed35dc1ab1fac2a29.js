178094c0519b9f45faba64a1911c81ac04d44b85
Jordi Gomis <jgc.jordi@gmail.com>
2019-12-27 13:14:05 +0100

    deploy

---
+++ precache-manifest.cf664892d205391ed35dc1ab1fac2a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79a10a4f5e13c3b9014",
+    "url": "/WebAppMyRoutesWithVue/css/app.8b350f36.css"
+  },
+  {
+    "revision": "4b81b080776cd27f2cd1",
+    "url": "/WebAppMyRoutesWithVue/css/chunk-vendors.115fe012.css"
+  },
+  {
+    "revision": "d24d6d071366569785b9210afe5d3ae1",
+    "url": "/WebAppMyRoutesWithVue/index.html"
+  },
+  {
+    "revision": "d79a10a4f5e13c3b9014",
+    "url": "/WebAppMyRoutesWithVue/js/app.a6924875.js"
+  },
+  {
+    "revision": "4b81b080776cd27f2cd1",
+    "url": "/WebAppMyRoutesWithVue/js/chunk-vendors.2e761464.js"
+  },
+  {
+    "revision": "6e6db2c878a1807132cda4b73a718e1b",
+    "url": "/WebAppMyRoutesWithVue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/WebAppMyRoutesWithVue/robots.txt"
+  }
+]);
